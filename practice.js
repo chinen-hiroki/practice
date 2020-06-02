84eca78de9184b2cb8af588c0c352858b16e2d80
chinen-hiroki <chinen.hiroki.2486@members.com>
2020-06-02 15:47:41 +0900

    Nodejs編集

--- practice.js
+++ practice.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+//jshint esversion:6
+
+//expressをロード
+const express = require("express");
+
+const app = express();
+
+//localhost:3000にアクセスされたときのRespond
+app.get("/", function(request, respond){
+    respond.send("hello world");
+});
+
+//localhost:3000/profileにアクセスされたときのRespond
+app.get("/profile", function(request, respond){
+    respond.send("My name is Chinen Hiroki.");
+});
+
+//listenで待ち受け状態
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
